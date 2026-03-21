c2fc510422c83a46299d2e8e7cada49a2a456607
Isabelly Franklin <isafranklin883@gmail.com>
2026-03-21 16:09:34 -0300

    atualizaçoes

--- js/calculo.js
+++ js/calculo.js
@@ -1,8 +1,12 @@
 const trilho = document.getElementById("trilho")
+const body = document.querySelector(`body`)
 
 trilho.addEventListener(`click`, ()=>{
     trilho.classList.toggle(`dark`)
+    body.classList.toggle(`dark`)
 })
+
+
 
 
 function calcular(){
